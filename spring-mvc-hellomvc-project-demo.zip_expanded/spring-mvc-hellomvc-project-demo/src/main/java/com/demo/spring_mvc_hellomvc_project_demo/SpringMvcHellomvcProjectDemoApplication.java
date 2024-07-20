5a1f87457ed3622737256fe55772f8e6fcf4c645
bpoongs <bpoongs@gmail.com>
2024-07-20 15:58:15 +0530

    SPRING MVC

---
+++ spring-mvc-hellomvc-project-demo.zip_expanded/spring-mvc-hellomvc-project-demo/src/main/java/com/demo/spring_mvc_hellomvc_project_demo/SpringMvcHellomvcProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_mvc_hellomvc_project_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHellomvcProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHellomvcProjectDemoApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-hellomvc-project-demo.zip_expanded/spring-mvc-hellomvc-project-demo/src/test/java/com/demo/spring_mvc_hellomvc_project_demo/SpringMvcHellomvcProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_mvc_hellomvc_project_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHellomvcProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
